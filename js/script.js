65c9aa097fc6b1e5762ef7c05604577ac720cbdf
Kirill Astafiev <AstafevK@yandex.ru>
2022-07-08 15:57:59 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,67 @@
+// Скрытие header'a
+
+var header = $('header'),
+    scrollPrev = 0;
+
+$(window).scroll(function () {
+    var scrolled = $(window).scrollTop();
+
+    if (scrolled > 65 && scrolled > scrollPrev) {
+        header.addClass('out');
+    } else {
+        header.removeClass('out');
+    }
+    scrollPrev = scrolled;
+});
+
+
+
+/* Бургер меню
+const menuIcon = document.querySelector(".menu__icon");
+const menuBody = document.querySelector(".menu__body");
+const headerLogo = document.querySelector(".header__logo");
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('active');
+    menuBody.classList.toggle('active');
+});
+
+document.querySelectorAll('.menu__link').forEach(n => n.
+    addEventListener('click', () => {
+        menuIcon.classList.remove('active');
+        menuBody.classList.remove('active');
+    }));
+
+window.onresize = function (e) {
+    if (menuIcon.classList.contains('active') && menuBody.classList.contains('active')) {
+        menuIcon.classList.remove('active');
+        menuBody.classList.remove('active');
+        console.log('1');
+    }
+};
+
+
+
+
+// Прокрутка при клике
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: 'smooth'
+            })
+            e.preventDefault();
+        }
+    }
+}
+*/
